bd5e4978e705c154a0fac0d7bad1a5eec62420c8
origin <wer2wer306@gmail.com>
2023-01-27 20:55:06 +0900

    interceptor

---
+++ src/main/java/hello/login/web/argumentresolver/Login.java
@@ -0,0 +1,11 @@
+package hello.login.web.argumentresolver;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME) // 어노테이션이 동작할때까지 남아있어야하게끔
+public @interface Login {
+}
